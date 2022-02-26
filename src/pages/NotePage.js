25b3ce45ef119e045e2f50c8bb345186ebb36bd7
Zenhar-Prodipto <zenhar.mz@gmail.com>
2022-02-26 21:56:09 +0600

    feat: implement update functionality on notes

--- src/pages/NotePage.js
+++ src/pages/NotePage.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { ReactComponent as ArrowLeft } from '../assests/arrow-left.svg';
 
-const NotePage = ({ match }) => {
+const NotePage = ({ match, history }) => {
   let noteId = match.params.id; //match.params will give me the url params. and id is what we specified in the url.
   let [note, setNote] = useState(null);
 
@@ -15,14 +15,40 @@ const NotePage = ({ match }) => {
     let data = await response.json();
     setNote(data);
   };
+
+  let updateNote = async () => {
+    fetch(`/api/notes/update/${noteId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(note),
+    });
+  };
+
+  let handleSubmit = () => {
+    updateNote();
+    history.push('/');
+  };
+
+  let handleChange = (value) => {
+    setNote((note) => ({ ...note, body: value }));
+    console.log('Handle Change:', note);
+  };
+
   return (
     <div className='note'>
       <div className='note-header'>
         <Link to='/'>
-          <ArrowLeft />
+          <ArrowLeft onClick={handleSubmit} />
         </Link>
       </div>
-      <textarea defaultValue={note?.body}></textarea>
+      <textarea
+        onChange={(e) => {
+          handleChange(e.target.value);
+        }}
+        value={note?.body}
+      ></textarea>
     </div>
   );
 };
